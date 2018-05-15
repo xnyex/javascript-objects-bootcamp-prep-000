4174621823d91342f63fd47dc31deae15bb1ddc9
Nye Smith <nyedess@gmail.com>
2018-05-15 14:24:03 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = {
+  Rhett: "Life Changes"
+  Halsey: "Roman Holiday"
+  Dev: "New"
+}
+function updatePlaylist(playlist, artist, title){
+  playlist.assign({artist: "title"})
+  return playlist
+}
